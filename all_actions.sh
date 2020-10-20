3e327d06c44ed8bd48655ae0987e17e7ac36018f
Jeremy Howard <j@fast.ai>
2020-10-20 13:23:05 -0700

    actions

--- all_actions.sh
+++ all_actions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in stdlib numpy pandas pytorch scipy h5py sphinx attrs sarge django
+for i in stdlib numpy pandas pytorch scipy sphinx django
 do
   ./action.sh $i $1
 done
